67347940bb8826d7ebac45c4723d10658206cf3b
Ulryx <39842628+Ulryx@users.noreply.github.com>
2025-06-11 07:48:17 -0500

    feat: add color feedback to rolls
    -Added color feedback to rolls

--- NatZeroHeroes/Assets/DiceRoller.cs
+++ NatZeroHeroes/Assets/DiceRoller.cs
@@ -16,5 +16,13 @@ public class DiceRoller : MonoBehaviour
     {
         int roll = Random.Range(1, 7);
         resultText.text = $"You rolled a {roll}";
+
+        // Color feedback
+        if (roll == 1)
+            resultText.color = Color.red;
+        else if (roll == 6)
+            resultText.color = new Color32(213, 160, 33, 255);
+        else
+            resultText.color = Color.white;
     }
 }
